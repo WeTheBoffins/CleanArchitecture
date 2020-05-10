f8a558a9db398d0c113b9bbb2be38bb6bb535b8c
Rahul Garg <techy.rahulgarg@gmail.com>
2020-05-10 17:43:51 +0530

    Add project structure and libraries

---
+++ CleanArchitecture.Test.Component/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace CleanArchitecture.Test.Component
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
